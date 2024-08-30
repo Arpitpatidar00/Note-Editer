7f65ac21b0277a4065674c093e7dcf1927877ea9
Arpit Patidar <123389729+Arpitpatidar00@users.noreply.github.com>
2024-08-30 17:13:33 +0530

    build update

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@material-ui/core";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
 import { theme } from "./Theme";
+import 'react-quill/dist/quill.snow.css';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
